3601ce19fab3dd4f89ed5c942865d00cd99e2e5a
Ivan Mizinov <mizinov.ivan@yandex.ru>
2025-12-01 13:05:53 +0300

    Initial commit

---
+++ src/test/java/ru/synergy/MusicStreamingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.synergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStreamingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
